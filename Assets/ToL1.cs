49ac1e7615c5e54a76deebc2d47a8f84633534c0
Yaksha <pig40873@gmail.com>
2022-11-14 00:25:28 +0800

    增加第一關規則的場景

--- Assets/ToL1.cs
+++ Assets/ToL1.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class StartControl : MonoBehaviour
+public class ToL1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -17,8 +16,8 @@ public class StartControl : MonoBehaviour
     {
         
     }
-   public void Click()
+    public void Click()
     {
-        SceneManager.LoadScene("L1_rule");
+        SceneManager.LoadScene("Level1");
     }
 }
