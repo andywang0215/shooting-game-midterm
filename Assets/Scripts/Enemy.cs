56cef25a44efbea79f5e1bfe4596957fedbcb05f
Yaksha <pig40873@gmail.com>
2022-12-22 01:17:11 +0800

    增加敵人行為

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,27 +1,175 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public EnemyStatus status;
+    private NavMeshAgent agent;
     public GameObject Player;
-    private CharacterController superman;
+    public Transform superman;
+    public GameObject bulletPrefab;
+    private IEnumerator fireCoroutine;
+    public Transform firePoint;
+    private bool isAttacking = false;
+    public float hp = 100;
+
     // Start is called before the first frame update
-    void Start()
+
+
+    public enum EnemyStatus
     {
-        superman = GetComponent<CharacterController>(); 
+        Idle, Chase, Attack, Escape, Heal, GetHeals
     }
 
+
+    void Start()
+    {
+        status = EnemyStatus.Idle;
+        agent = GetComponent<NavMeshAgent>();
+        fireCoroutine = FireBullet();
+
+    }
     // Update is called once per frame
     void Update()
     {
-        //計算和玩家的距離
-        Vector3 dir = Player.transform.position- transform.position;
+            float d = Vector3.Distance(transform.position, superman.transform.position);
+           
+            // Idle (閒置) 
+            if (status == EnemyStatus.Idle)
+            {
+                // 往 Chase 判斷
+                if (d < 10)
+                {
+                    status = EnemyStatus.Chase;
+                    return;
+                }
+
+                
+
+                // 行為
+                Idle();
+            }
+
+            // Chase (追逐)
+            if (status == EnemyStatus.Chase)
+            {
+                // 往 Idle 的判斷
+                if (d > 12)
+                {
+                    status = EnemyStatus.Idle;
+                    return;
+                }
+                if (d < 5)
+                {
+                   status = EnemyStatus.Attack;
+                   return;
+                }
+
+
+            // 行為
+            Chase();
+            }
+
+           
+
+            // Escape (逃跑)
+            if (status == EnemyStatus.Escape)
+            {
+                Escape();
+            }
+
+           if (status == EnemyStatus.Attack)
+           {
+            // 往 Chase 的判斷
+              if (d > 5)
+              {
+                status = EnemyStatus.Chase;
+                StopCoroutine(fireCoroutine);
+                isAttacking = false;
+                return;
+              }
+
+            
+
+              // 行為
+              Attack();
+           }
 
 
 
-        //move
-        superman.Move(dir.normalized * -5f * Time.deltaTime);
     }
-  
+
+    // 狀態行為：閒置
+    private void Idle()
+        {
+            agent.isStopped = true;
+        }
+
+        // 狀態行為：追逐
+        private void Chase()
+        {
+            agent.isStopped = false;
+            agent.SetDestination(superman.position);
+        }
+
+    // 狀態行為：攻擊
+    private void Attack()
+    {
+        agent.isStopped = true;
+
+        // 看向目標
+        transform.LookAt(superman);
+
+        // 只執行一次，開始重複射擊
+        if (!isAttacking)
+        {
+            StartCoroutine(fireCoroutine);
+            isAttacking = true;
+        }
+    }
+
+    IEnumerator FireBullet()
+    {
+        // 無窮迴圈
+        while (true)
+        {
+            // // 產生出子彈
+            Instantiate(bulletPrefab, firePoint.transform.position, transform.rotation);
+
+            // 停兩秒
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void Escape()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(superman.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 當被子彈攻擊
+        if (other.tag == "Bullet")
+        {
+            hp = hp - 10;
+
+            if (hp < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+      
+    }
+
+
+
+
+
 }
+
+
+
